a90cd6b43efd246384bb7ba9c41668cfc91a448f
azharjk <azhr.project@gmail.com>
2022-06-20 11:57:40 +0700

    Add products controller
    But only listing product is implemented

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Product;
+use App\Http\Resources\ProductResource;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ProductResource::collection(Product::all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return 'FIXME: Implement show for products';
+    }
+}
