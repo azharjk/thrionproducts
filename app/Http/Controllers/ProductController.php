a5c6142a9c049d6108bc107d6c99eef792256995
azharjk <azhr.project@gmail.com>
2022-06-27 16:12:37 +0700

    Add product detail endpoint

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -25,8 +25,9 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(int $id)
     {
-        return 'FIXME: Implement show for products';
+        $product = Product::findOrFail($id);
+        return new ProductResource($product);
     }
 }
